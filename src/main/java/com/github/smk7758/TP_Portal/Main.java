7ea0b4821b55c0dc6ba0385212687f689ad92fb3
smk7758 <maxkovko@yahoo.co.jp>
2017-08-22 00:42:47 +0900

    teleport出来るまで終了 = 0.0.1完
    teleport出来るまでになったでのver: 0.0.1がこれで終わりです。
    まだtodoがたくさんあるので更新していきたい。

--- src/main/java/com/github/smk7758/TP_Portal/Main.java
+++ src/main/java/com/github/smk7758/TP_Portal/Main.java
@@ -27,51 +27,71 @@ public class Main extends JavaPlugin implements Listener {
 
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
+		saveDefaultConfig();
+		reloadConfig();
+		DebugMode = getConfig().getBoolean("DebugMode");
 	}
 
 	public void onDisable() {
-
 	}
 
-	public Location getPortalLocation(String path) {
+	public boolean validatePortalFromConfig(String portal_name) {
+		String path = "PortalList." + portal_name;
+		reloadConfig();
+		if (getConfig().contains(path + ".World") && getConfig().contains(path + ".X") && getConfig().contains(path + ".Y")
+				&& getConfig().contains(path + ".Z")) return true;
+		else return false;
+	}
+
+	public void savePortalSignLocationFromConfig(String portal_name, Sign sign) {
+		String path = "PortalList." + portal_name;
+		getConfig().set(path + ".World", sign.getWorld().getName());
+		getConfig().set(path + ".X", sign.getX());
+		getConfig().set(path + ".Y", sign.getY());
+		getConfig().set(path + ".Z", sign.getZ());
+		saveConfig();
+	}
+
+	// みかん味
+	public Location loadPortalSignLocationFromConfig(String portal_name) {
+		String path = "PortalList." + portal_name;
+		reloadConfig();
 		World pl_world = getServer().getWorld(getConfig().getString(path + ".World"));
-		double pl_x = getConfig().getDouble(path + ".X");
-		double pl_y = getConfig().getDouble(path + ".Y");
-		double pl_z = getConfig().getDouble(path + ".Z");
-		float pl_Yaw = (float) getConfig().getDouble(path + ".Yaw");
-		float pl_Pitch = (float) getConfig().getDouble(path + ".Pitch");
-		return new Location(pl_world, pl_x, pl_y, pl_z, pl_Yaw, pl_Pitch);
+		int pl_x = getConfig().getInt(path + ".X");
+		int pl_y = getConfig().getInt(path + ".Y");
+		int pl_z = getConfig().getInt(path + ".Z");
+		return new Location(pl_world, pl_x, pl_y, pl_z);
 	}
 
-	public void tpPlayerToPortal(Player player, String portal_name) {
-		Location loc_portal = getPortalLocation("XXXXX"); // みかん味
-		if (player.getLocation().getBlockX() == 0 && player.getLocation().getBlockY() == 0 && player.getLocation().getBlockZ() == 0) {
-			player.teleport(loc_portal);
-		}
-	}
+	//何に使ってるんですか！？↓
+//	public void tpPlayerToPortal(Player player, Location portal_loc) {
+//	}
 
+	// todo: 方向違いに対応する。
+	// todo: 空白部分に対応する。
 	public boolean checkPortal(Sign sign) {
-		//ゴリ押しだが、こういくのしかないのかぁと思ってやってしまった。
-		//とりあえず、Block型にしてそこから型にはめてみた。
+		// ゴリ押しだが、こういくのしかないのかぁと思ってやってしまった。
+		// とりあえず、Block型にしてそこから型にはめてみた。
 		org.bukkit.material.Sign s = (org.bukkit.material.Sign) sign.getBlock().getState().getData();
 		Block attachedBlock = sign.getBlock().getRelative(s.getAttachedFace());
-		//起点を取得。左上とする。
-		int starting_point_loc[] = {attachedBlock.getX(), attachedBlock.getY()+2, attachedBlock.getZ()};
-//		int decrease_number_x = 0;
-//		if (s.getFacing().equals(BlockFace.NORTH)) decrease_number_x = 0;
-//		else if (s.getFacing().equals(BlockFace.EAST)) decrease_number_x = 0;
-//		else if (s.getFacing().equals(BlockFace.SOUTH)) decrease_number_x = 0;
-//		else if (s.getFacing().equals(BlockFace.WEST)) decrease_number_x = 0;
+		// 起点を取得。左上とする。
+		int starting_point_loc[] = { attachedBlock.getX(), attachedBlock.getY() + 2, attachedBlock.getZ() };
+		// int decrease_number_x = 0;
+		// if (s.getFacing().equals(BlockFace.NORTH)) decrease_number_x = 0;
+		// else if (s.getFacing().equals(BlockFace.EAST)) decrease_number_x = 0;
+		// else if (s.getFacing().equals(BlockFace.SOUTH)) decrease_number_x = 0;
+		// else if (s.getFacing().equals(BlockFace.WEST)) decrease_number_x = 0;
 		World world = sign.getWorld();
-		//Y軸方向
-		for (int i=0; i<5; i++) {
-			//X軸方向
-			for (int j=0; j<4; j++) {
-				Location loc0 = new Location(world, starting_point_loc[0]+j, starting_point_loc[1]-i, starting_point_loc[2]);
+		// Y軸方向
+		for (int i = 0; i < 5; i++) {
+			// X軸方向
+			for (int j = 0; j < 4; j++) {
+				Location loc0 = new Location(world, starting_point_loc[0] + j, starting_point_loc[1] - i, starting_point_loc[2]);
 				cLog.debug(loc0.toString());
 				if (!loc0.getBlock().getType().equals(Material.IRON_BLOCK)) return false;
 			}
 		}
+		cLog.debug("checkPortal!");
 		return true;
 	}
 
@@ -79,29 +99,35 @@ public class Main extends JavaPlugin implements Listener {
 	public void onClick(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
 		Block block = event.getClickedBlock();
-		//看板かの判別
-		if (block == null || !(block.getType().equals(Material.SIGN_POST) || block.getType().equals(Material.WALL_SIGN) || block.getType().equals(Material.SIGN))) return;
+		// 看板かの判別
+		if (block == null || !(block.getType().equals(Material.SIGN_POST) || block.getType().equals(Material.WALL_SIGN)
+				|| block.getType().equals(Material.SIGN))) return;
 		cLog.debug("Pass: sign");
-		Sign sign = (Sign)block.getState();
-		//[Portal] の判別]
+		Sign sign = (Sign) block.getState();
+		// [Portal] の判別]
 		if (!sign.getLine(0).equalsIgnoreCase("[Portal]")) return;
 		cLog.debug("Pass: [Portal]");
 		String portal_name = sign.getLine(1);
-		if (portal_name == null || portal_name.isEmpty()) {
-			cLog.sendMessage(player, "Please, write the portal name.", 0);
-			return;
-		}
-//		if (getConfig().contains(portal_name)) { //containsをしっかりと判断するものに変更すべき。
+		if (portal_name != null && !portal_name.isEmpty()) {
 			if (checkPortal(sign)) {
 				cLog.debug("Pass: checkPortal");
-				tpPlayerToPortal(player, portal_name);
+				if (validatePortalFromConfig(portal_name)) {
+					player.teleport(loadPortalSignLocationFromConfig(portal_name));
+					cLog.debug("Pass; teleport!!");
+					//todo: 相手側のポータルの判定。
+					return;
+				} else {
+					// 存在しなかったので、作成する。
+					savePortalSignLocationFromConfig(portal_name, sign);
+					cLog.debug("Made portal location to config.");
+				}
 			} else {
-				//ポータルの基準を満たしていない。
-				cLog.sendMessage(player, "The portal is not good.", 2);
+				// ポータルの基準を満たしていない。
+				cLog.sendMessage(player, "The portal is not properly.", 2);
 			}
-//		} else {
-//			//存在しなかったので、作成する。
-//			//if (getConfig().getString(""))
-//		}
+		} else {
+			cLog.sendMessage(player, "Please, write the portal name.", 0);
+		}
+		return;
 	}
 }
